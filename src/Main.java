0404fdcb4234752120feb31548e4581a7fbe0e25
Wall1EMone <mone.larsson@outlook.com>
2025-10-23 12:14:37 +0200

    lagtTillOlikeVal

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        boolean run = true;
+
+        while(run){
+            System.out.println("Välkommen till Mones golfshop");
+            System.out.println("Gör ditt val");
+            System.out.println();
+            System.out.println("1.");
+            System.out.println("2.");
+            System.out.println("3.");
+            System.out.println("4.");
+            System.out.println("5. Avsluta");
+
+            int choice = scanner.nextInt();
+
+            if(choice == 5){
+                System.out.println("Tack och välkommen åter!");
+                scanner.nextLine();
+                break;
+            }
+        }
+
+
+
     }
 }
